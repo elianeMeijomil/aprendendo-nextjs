77fa316bf28b863e0089717f673211431f40e01f
Eliane Meijomil <soul@elianemeijomil.com>
2022-12-21 23:59:39 -0300

    disponibilizando links na página principal

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,15 @@ function Home(){
         <div>
             <h1>Home</h1>
             <p>Bem vindo ao meu primeiro componente React</p>
-            <p>
+            <h2></h2>
+            <ul>
+                <li>
                 <Link href="products/">Products</Link>
-            </p>
+                </li>
+                <li>
+                <Link href="reviews/">Reviews</Link>
+                </li>
+            </ul>
         </div>
     );
 }
