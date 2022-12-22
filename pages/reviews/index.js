b9a5421a0a67a5b31aa478023dddb0b3d8822d2d
Eliane Meijomil <soul@elianemeijomil.com>
2022-12-21 23:58:55 -0300

    corrigindo links de reviews

--- pages/reviews/index.js
+++ pages/reviews/index.js
@@ -7,16 +7,16 @@ function Reviews(){
             <h2>Product list</h2>
             <ul>
                 <li>
-                    <Link href="products/1">Product #1</Link>
+                    <Link href="reviews/1">Product #1</Link>
                 </li>
                 <li>
-                    <Link href="products/2">Product #2</Link>
+                    <Link href="reviews/2">Product #2</Link>
                 </li>
                 <li>
-                    <Link href="products/3">Product #3</Link>
+                    <Link href="reviews/3">Product #3</Link>
                 </li>
                 <li>
-                    <Link href="products/4">Product #4</Link>
+                    <Link href="reviews/4">Product #4</Link>
                 </li>
             </ul>
         </div>
