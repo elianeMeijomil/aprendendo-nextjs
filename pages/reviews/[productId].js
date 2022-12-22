150e7c4f7f60ba921eadc3f7700ac5a2684f08ad
Eliane Meijomil <soul@elianemeijomil.com>
2022-12-21 22:28:06 -0300

    adicionando review de produtos com o productId processado em serverSide

---
+++ pages/reviews/[productId].js
@@ -0,0 +1,35 @@
+import Link from "next/link"
+export async function getServerSideProps(context) {
+    const productId = context.query.productId;
+    return {
+        props:{
+            productId:productId,
+            productImg:"/imgs/products/product-"+productId+".png"
+        }
+    }
+}
+
+function Review(props){
+    return (
+    <div> 
+        <h1>Product #{props.productId} Review</h1>
+        <img src={props.productImg} />
+        <ul>
+                <li>
+                    <Link href="1">Product #1</Link>
+                </li>
+                <li>
+                    <Link href="2">Product #2</Link>
+                </li>
+                <li>
+                    <Link href="3">Product #3</Link>
+                </li>
+                <li>
+                    <Link href="4">Product #4</Link>
+                </li>
+            </ul>
+            <p>Esta página processa o productId serverSide</p>
+    </div>)
+}
+
+export default Review
